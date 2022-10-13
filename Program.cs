84fe56405529403b6761345703266c865deca73d
Дарья Шарипова <darya.sharipova@oldcodex.ru>
2022-10-13 16:59:21 +0500

    сделано третье задание

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿//17
+Console.WriteLine("Введите четырехзначное число:");
+int x = int.Parse(Console.ReadLine());
+int a = (x/1000);
+int b = (x/100)%10;
+int c = (x/10)%10;
+int d = x%10;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
+Console.WriteLine(a);
+Console.ReadKey();
